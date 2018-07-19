d0bb10c6bbd5c767e9d3899b872e478f34742d1d
Jhonny Bravo <sanfranceshika5@gmail.com>
2018-07-19 22:12:19 +0900

    modified: WebContent/js/FileController.js
    browserify を導入。
    
    インデントサイズを半角スペース 2 つに変更。

--- WebContent/js/FileController.js
+++ WebContent/js/FileController.js
@@ -1,85 +1,92 @@
 /**
  * ファイル出力処理を管理する。
  */
-var FileController = function() {
-	var privateObjMdIB = new MdInputBean();
-	var privateStrTitle = privateObjMdIB.getTitle();
+var FileController = function () {
+  /**
+   * ファイルを生成して出力する。
+   * 
+   * @param strName
+   *            出力するファイル名を指定する。
+   * @param strContents
+   *            ファイルへ書込む内容を指定する。
+   * @param strType
+   *            ファイルの種類を指定する。(text/plain, text/html, etc...)
+   */
+  function privateSaveFile(strName, strContents, strType) {
+    var objPattern = /\n/g;
+    var objBlob = new Blob([strContents.replace(objPattern, "\r\n")], {
+      type: strType
+    });
+    var objURL = URL.createObjectURL(objBlob);
 
-	/**
-	 * ファイルを生成して出力する。
-	 * 
-	 * @param strName
-	 *            出力するファイル名を指定する。
-	 * @param strContents
-	 *            ファイルへ書込む内容を指定する。
-	 * @param strType
-	 *            ファイルの種類を指定する。(text/plain, text/html, etc...)
-	 */
-	function privateSaveFile(strName, strContents, strType) {
-		var objBlob = new Blob([ strContents ], {
-			type : strType
-		});
-		var objURL = URL.createObjectURL(objBlob);
+    var objAnchor = document.createElement("a");
+    objAnchor.href = objURL;
+    objAnchor.target = "_blank";
+    objAnchor.download = strName;
+    objAnchor.click();
 
-		var objAnchor = document.createElement("a");
-		objAnchor.href = objURL;
-		objAnchor.target = "_blank";
-		objAnchor.download = strName;
-		objAnchor.click();
+    URL.revokeObjectURL(objURL);
+  }
 
-		URL.revokeObjectURL(objURL);
-	}
+  /**
+   * input#txtTitle と textarea#txtContents から入力を受取り、 txt ファイルへ出力する。
+   */
+  function publicSaveMd() {
+    var objMdIB = require("./MdInputBean");
 
-	/**
-	 * input#txtTitle と textarea#txtContents から入力を受取り、 txt ファイルへ出力する。
-	 */
-	function publicSaveMd() {
-		var strContents = privateObjMdIB.getContents();
-		var strMdInput = "# " + privateStrTitle + "\n" + strContents;
-		privateSaveFile(privateStrTitle + ".txt", strMdInput, "text/plain");
-	}
+    var strTitle = objMdIB.getTitle();
+    var strContents = objMdIB.getContents();
 
-	/**
-	 * div#HtmlOutput から innerHTML を取得し、 html ファイルへ出力する。
-	 */
-	function publicSaveHtml() {
-		// ベースオブジェクトの生成。
-		var objHtml = document.createElement("html");
+    var strMdInput = "# " + strTitle + "\n" + strContents;
+    privateSaveFile(strTitle + ".txt", strMdInput, "text/plain");
+  }
 
-		var objHead = document.head.cloneNode(true);
-		var objTitle = objHead.getElementsByTagName("title");
+  /**
+   * div#HtmlOutput から innerHTML を取得し、 html ファイルへ出力する。
+   */
+  function publicSaveHtml() {
+    // ベースオブジェクトの生成。
+    var objHtml = document.createElement("html");
 
-		for (var i = 0; i < objTitle.length; i++) {
-			objTitle[i].textContent = privateStrTitle;
-		}
+    var objHead = document.head.cloneNode(true);
+    var objTitle = objHead.getElementsByTagName("title");
 
-		objHtml.appendChild(objHead);
+    var objMdIB = require("./MdInputBean");
+    var strTitle = objMdIB.getTitle();
 
-		var objBody = document.body.cloneNode(true);
-		objHtml.appendChild(objBody);
+    for (var i = 0; i < objTitle.length; i++) {
+      objTitle[i].textContent = strTitle;
+    }
 
-		// 余分な要素を削除。
-		var objNav = objHtml.getElementsByTagName("nav");
+    objHtml.appendChild(objHead);
 
-		for (var i = 0; i < objNav.length; i++) {
-			objNav[i].remove();
-		}
+    var objBody = document.body.cloneNode(true);
+    objHtml.appendChild(objBody);
 
-		var objDiv = objHtml.getElementsByTagName("div");
+    // 余分な要素を削除。
+    var objNav = objHtml.getElementsByTagName("nav");
 
-		for (var i = 0; i < objDiv.length; i++) {
-			if (objDiv[i].id == "MdInput" || objDiv[i].id == "bin") {
-				objDiv[i].remove();
-			}
-		}
+    for (var i = 0; i < objNav.length; i++) {
+      objNav[i].remove();
+    }
 
-		// ファイル出力
-		var strHtmlOutput = objHtml.innerHTML;
-		privateSaveFile(privateStrTitle + ".html", strHtmlOutput, "text/html");
-	}
+    var objDiv = objHtml.getElementsByTagName("div");
 
-	return {
-		saveMd : publicSaveMd,
-		saveHtml : publicSaveHtml
-	}
-};
+    for (var i = 0; i < objDiv.length; i++) {
+      if (objDiv[i].id == "MdInput" || objDiv[i].id == "bin") {
+        objDiv[i].remove();
+      }
+    }
+
+    // ファイル出力
+    var strHtmlOutput = objHtml.innerHTML;
+    privateSaveFile(strTitle + ".html", strHtmlOutput, "text/html");
+  }
+
+  return {
+    saveMd: publicSaveMd,
+    saveHtml: publicSaveHtml
+  };
+}();
+
+module.exports = FileController;
