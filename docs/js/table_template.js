8ad9dc180393ab4f0b9b04140039077368efa47a
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-06-11 23:42:29 +0900

    new file: docs/js/table_template.js

---
+++ docs/js/table_template.js
@@ -0,0 +1,9 @@
+var entityTable = {
+    template : "#entity-table-template",
+    props : [ "file-name", "table-name" ]
+}
+
+var definitionTable = {
+    template : "#definition-table-template",
+    props : [ "definitions" ]
+}
