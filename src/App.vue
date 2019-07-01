632d1f039cfc7ba305120c72f977c3517d0f9fcb
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-07-01 21:30:59 +0900

    new file: src/App.vue

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <Navbar/>
+    <md-editer/>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import Navbar from './components/Navbar.vue';
+import MdEditer from './components/MdEditer.vue';
+
+export default Vue.extend({
+  name: 'app',
+  components: {
+    Navbar,
+    MdEditer,
+  },
+});
+</script>
