f3b707a361925e68cd3bea18e7dcf5ede43b8000
Jhonny Bravo <sanfranceshika5@gmail.com>
2019-07-01 21:30:18 +0900

    new file: src/components/Navbar.vue

---
+++ src/components/Navbar.vue
@@ -0,0 +1,34 @@
+<template>
+    <b-navbar toggleable="lg" type="dark" variant="dark">
+        <b-navbar-brand :href="brand.url">{{brand.title}}</b-navbar-brand>
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+        <b-collapse id="nav-collapse" is-nav>
+            <b-navbar-nav>
+                <b-nav-item v-for="link in links" :key="link.url" :href="link.url">{{link.title}}</b-nav-item>
+            </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</template>
+<script lang="ts">
+import Vue from 'vue';
+import BootstrapVue from 'bootstrap-vue';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+Vue.use(BootstrapVue);
+
+export default Vue.extend({
+    data() {
+        return {
+            brand: {
+                url: '/',
+                title: 'Mark Down Editor',
+            },
+            links: [{
+                url: '/',
+                title: 'Home',
+            }],
+        };
+    },
+});
+</script>
